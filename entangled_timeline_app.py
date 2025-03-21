d6a10201860203c51d9b0ba0c92ab141c6ff2575
Jessica McGlothern <182583049+idotamdot@users.noreply.github.com>
2025-03-21 16:37:56 -0500

    Update entangled_timeline_app.py

--- entangled_timeline_app.py
+++ entangled_timeline_app.py
@@ -1,6 +1,6 @@
 # Streamlit app file - previously created
 # if __name__ == "__main__":
-    import os
+import os
     port = int(os.environ.get("PORT", 8501))  # Change default from 8501
     st.run(port=port, host='0.0.0.0')
 
