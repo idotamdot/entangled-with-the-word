fb69533dafedc5269070b425f065a9f5dc5a462e
Jessica McGlothern <182583049+idotamdot@users.noreply.github.com>
2025-03-22 06:37:59 -0500

    Update entangled_timeline_app.py

--- entangled_timeline_app.py
+++ entangled_timeline_app.py
@@ -4,5 +4,6 @@ import streamlit as st
 port = int(os.environ.get("PORT", 8000))  # Azure expects port 8000
 st.set_page_config(page_title="Entangled with the Word")
 
+
 # Your app content here...
 st.title("Entangled with the Word")
