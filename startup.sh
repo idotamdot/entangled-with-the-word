d1ceff0f1727217efec9c0207d30ea264a07639f
Jessica McGlothern <182583049+idotamdot@users.noreply.github.com>
2025-03-22 06:41:24 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# Upgrade pip and install dependencies
-python -m pip install --upgrade pip
+pip install --upgrade pip
 pip install -r requirements.txt
-
-# Run Streamlit app
-exec streamlit run entangled_timeline_app.py --server.port=$PORT --server.address=0.0.0.0
+streamlit run entangled_timeline_app.py --server.port=$PORT --server.address=0.0.0.0
