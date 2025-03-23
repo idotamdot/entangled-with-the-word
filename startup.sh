945242a0c33e35556ec055df54f1768d96682425
Jessica McGlothern <182583049+idotamdot@users.noreply.github.com>
2025-03-23 09:15:06 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m streamlit run entangled_timeline_app.py --server.port=$PORT --server.address=0.0.0.0
+streamlit run entangled_timeline_app.py --server.port=$PORT --server.address=0.0.0.0
